7816d8448be34c13901d7043e0c183eb81640372
pubes.praidee <pubes.praidee@gmail.com>
2019-10-22 04:26:25 +0700

    RE:Structure

--- next.config.js
+++ next.config.js
@@ -1,41 +1,9 @@
-//https://github.com/zeit/next.js/issues/3131
-
-const path = require('path')
-const glob = require('glob')
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
-
-module.exports = {
-	webpack: (config, { dev }) => {
-		config.plugins.push(new webpack.EnvironmentPlugin(localEnv)),
-		config.module.rules.push(
-			{
-				test: /\.(css|scss)/,
-				loader: 'emit-file-loader',
-				options: {
-					name: 'dist/[path][name].[ext]'
-				}
-			},
-			{
-				test: /\.css$/,
-				use: ['babel-loader', 'raw-loader', 'postcss-loader']
-			},
-			{
-				test: /\.s(a|c)ss$/,
-				use: ['babel-loader', 'raw-loader',
-					{
-						loader: 'sass-loader',
-						options: {
-							includePaths: ['styles', 'node_modules']
-								.map((d) => path.join(__dirname, d))
-								.map((g) => glob.sync(g))
-								.reduce((a, c) => a.concat(c), [])
-						}
-					}
-				]
-			}
-		)
-
-		return config
-	}
-}
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+    distDir: 'build',
+    pageExtensions: ['mdx', 'jsx', 'js'],
+    cssModules: true,
+    env: {
+        customKey: 'value',
+    },
+})
--- redux-reducer/SomeReducer.js
+++ redux-reducer/SomeReducer.js
@@ -1,5 +1,5 @@
 const defaultState = {
-	someState: [],
+	someState: [1,2,3,4,5,6,7,8,9,10],
 }
 
 const SomeReducer = (state = defaultState, action) => {
