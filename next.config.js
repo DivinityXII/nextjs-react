80b4570cbd801f51f69f347b8f2c424fc7a267ab
pubes.praidee <pubes.praidee@gmail.com>
2019-10-22 05:17:28 +0700

    no message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
-    distDir: 'build',
+    targets: 'serverless',
     pageExtensions: ['mdx', 'jsx', 'js'],
     cssModules: true,
     env: {
